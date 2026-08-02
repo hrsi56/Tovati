170c71ddc458d03fcca103b3c88e2aa2b8fc02c3
Yarden Viktor Dejorno <hrsi56@gmail.com>
2026-08-02 19:01:00 +0300

    Release Tovati 1.7.1 with neutral thermometer wording

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.yv.bbttracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.7.0"
+        versionCode = 15
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
